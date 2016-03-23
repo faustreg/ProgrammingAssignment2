6e5451fcce1a413cdfa28a9c3e26528a2ff4a931
faustreg <faust@online.ua>
2016-03-23 18:34:03 +0200

    Programmimg Assignment 2
    2 functions to calculate inverse matrix or read the inverse from cache
    if it was calculated before.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The first function makeCacheMatrix takes a square matrix (which should have inverse matrix)
+## and creates special 'matrix', saving it to the cache.
+## 
+## The second function cacheSolve calculates the inverse to the matrix inputed to makeCacheMatrix
+## or returns it from cache if it was calculated before.
 
-## Write a short comment describing this function
+## The function makeCacheMatrix creates a special "matrix" (of List type)
+## which containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse matrix
+## 4. get the value of the inverse matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x) {
+  M <- NULL
+  set <- function(y) {
+    x <<- y
+    M <<- NULL
+  }
+  get <- function() x
+  setMatrix <- function(solve) M <<- solve
+  getMatrix <- function() M
+  list(set = set, get = get,
+       setMatrix = setMatrix,
+       getMatrix = getMatrix)
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve returns the inverse matrix 
+## of the special "matrix created with the function makeCacheMatrix.
+## It checks if the inverse was already been calculated (for defined matrix)
+## If the inverse were calculated before, it gets the inverse matrix from 
+## the cache and skips calculation, returning also the string 'getting cached data'
+## Otherwise it calculates the inverse matrix and returns it.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  M <- x$getMatrix()
+  if(!is.null(M)) {
+    message("getting cached data")
+    return(M)
+  }
+  data <- x$get()
+  M <- solve(data, ...)
+  x$setMatrix(M)
+  M
 }
---
+++ example.R
@@ -0,0 +1,34 @@
+## set the value of the vector
+## get the value of the vector
+## set the value of the mean
+## get the value of the mean
+
+makeVector <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmean <- function(mean) m <<- mean
+  getmean <- function() m
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
+}
+
+
+
+## calculates the mean or gets it from cache if it's already calculated
+
+cachemean <- function(x, ...) {
+  m <- x$getmean()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- mean(data, ...)
+  x$setmean(m)
+  m
+}
